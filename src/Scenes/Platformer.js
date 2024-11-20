eb0597ad0e368d9e8cb525424db4721ccc2a6a36
Jason Li <Jason.Li8885551@gmail.com>
2024-11-19 21:19:55 -0800

    Added Phaser

---
+++ src/Scenes/Platformer.js
@@ -0,0 +1,17 @@
+class Platformer extends Phaser.Scene {
+    constructor() {
+        super("platformerScene");
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    // Never get here since a new scene is started in create()
+    update() {
+    }
+}
